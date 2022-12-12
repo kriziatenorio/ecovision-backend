2d3252985102a6cc9f94e1c5313d28c67e2e0df8
Krizia Tenorio <kriziatenorio@gmail.com>
2022-12-12 20:06:22 +0800

    testing registration of user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -17,42 +17,42 @@ const salt = 10
 // * utilities
 const fixedName = require('../helpers/util')
 
-// router.post('/register', (req, res) => {
-//     let data = req.body
-//     let name = fixedName(data.name) // * normalize name from "john doe" to "John Doe"
-//     let password = data.password; // bcrypt.hashSync(data.password, salt) // * hash password here
+router.post('/register', async (req, res) => {
+    let data = req.body
+    let name = fixedName(data.name) // * normalize name from "john doe" to "John Doe"
+    let password = data.password; // bcrypt.hashSync(data.password, salt) // * hash password here
 
-//     createUserWithEmailAndPassword(auth, data.email, password)
-//         .then(userCredential => {
-//             const postRef = db.ref(`users/${userCredential.user.uid}`)
+    await createUserWithEmailAndPassword(auth, data.email, password)
+        .then(userCredential => {
+            const postRef = db.ref(`users/${userCredential.user.uid}`)
 
-//             postRef.set({ name: name, createdAt: Date.now() })
-//                 .then(() => {
-//                     res.status(200).json({ 
-//                         success: true, 
-//                         message: `${name} is registered successfully`,
-//                         data: userCredential
-//                     })
-//                 })
-//                 .catch(err => res.status(404).json({ success: false, message: err }))
-//         })
-//         .catch(err => res.json(err))
+            postRef.set({ name: name, createdAt: Date.now() })
+                .then(() => {
+                    res.status(200).json({ 
+                        success: true, 
+                        message: `${name} is registered successfully`,
+                        data: userCredential
+                    })
+                })
+                .catch(err => res.status(404).json({ success: false, message: err }))
+        })
+        .catch(err => res.json(err))
 
-//     // admin.auth().createUser({
-//     //     email: data.email,
-//     //     emailVerified: true,
-//     //     password: password,
-//     //     displayName: name
-//     // }).then(record => {
-//     //     const postRef = db.ref(`users/${record.uid}`)
+    // admin.auth().createUser({
+    //     email: data.email,
+    //     emailVerified: true,
+    //     password: password,
+    //     displayName: name
+    // }).then(record => {
+    //     const postRef = db.ref(`users/${record.uid}`)
 
-//     //     postRef.set({ name: name, createdAt: Date.now() })
-//     //         .then(() => {
-//     //             res.status(200).json({ success: true, message: `${name} is registered successfully` })
-//     //         })
-//     //         .catch(err => res.status(404).json({ success: false, message: err }))
-//     // }).catch(err => res.status(500).json(err))
-// });
+    //     postRef.set({ name: name, createdAt: Date.now() })
+    //         .then(() => {
+    //             res.status(200).json({ success: true, message: `${name} is registered successfully` })
+    //         })
+    //         .catch(err => res.status(404).json({ success: false, message: err }))
+    // }).catch(err => res.status(500).json(err))
+});
 
 // // * LOGIN USER
 // router.post('/login', async (req, res) => {
