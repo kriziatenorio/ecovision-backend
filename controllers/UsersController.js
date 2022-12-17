6edb4d6780fc8a27e9d6e6f0088650b19e98bd1d
Krizia Tenorio <kriziatenorio@gmail.com>
2022-12-18 00:51:33 +0800

    fixed return in login

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -60,7 +60,6 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
     let data = req.body
 
-    res.json(data)
     await signInWithEmailAndPassword(auth, data.email, data.password)
         .then(userCredential => {
             res.json({
