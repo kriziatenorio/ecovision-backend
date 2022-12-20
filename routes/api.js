a3b02449a65d57b2546af9a6e898380d40322006
Krizia Tenorio <kriziatenorio@gmail.com>
2022-12-20 13:41:27 +0800

    fixed listings

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require("express")
 const server = express()
 
 // Routes
-const listingsController = require('../controllers/listingsController')
+const listingsController = require('../controllers/ListingsController')
 const usersController = require('../controllers/UsersController')
 server.use('/api/listings', listingsController)
 server.use('/api/users', usersController)
